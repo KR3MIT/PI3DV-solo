f48174403537452bac0043ed9a15c2fdd96bf109
KR3MIT <jason.cm.hassan@gmail.com>
2024-12-02 14:26:00 +0100

    idk

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,7 +2,52 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public enum State
+    {
+        Idle,
+        Move,
+        Attack
+    }
     public float health = 100;
+    private State currentState;
+    [SerializeField] private Animator anim;
+    [SerializeField] private float range = 50;
+    [SerializeField] private State state;
+    void Start()
+    {
+        currentState = State.Idle;
+    }
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.Idle:
+                Idle();
+                break;
+            case State.Move:
+                Move();
+                break;
+            case State.Attack:
+                Attack();
+                break;
+        }
+    }
+    void Idle()
+    {
+        
+        //currentState = State.Move;
+    }
+    void Move()
+    {
+        
+        //currentState = State.Attack;
+    }
+    void Attack()
+    {
+        
+        //currentState = State.Move;
+    }
+        
     public void TakeDamage(float _damage)
     {
         health -= _damage;
